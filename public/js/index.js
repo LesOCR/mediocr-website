798255b6950938ca8a7680efa8f09c1307372552
Manuel HUEZ <manuel.huez@gmail.com>
2014-10-08 18:53:49 +0200

    Worked a bit more on the animation of the logo

--- public/js/index.js
+++ public/js/index.js
@@ -10,16 +10,26 @@
   {
       $animateLogo = jQuery('<div/>', {
           class: 'logo-image-animate',
+          style: 'opacity: 0'
       });
       $animateLogo.appendTo('.logo-container').animate({
-         opacity: 0,
-         height: "168",
-         width: "168",
-         top: "0",
-         left: "0"
-     }, 1000, function()
+         opacity: 0.6,
+         height: "68",
+         width: "68",
+         top: "50",
+         left: "50"
+     }, 200, function()
      {
-         $animateLogo.remove();
+         $animateLogo.animate({
+            opacity: 0,
+            height: "64",
+            width: "40",
+            top: "50%",
+            left: "50%"
+        }, 300, function()
+        {
+            $animateLogo.remove();
+        });
      });
   }
 })();
