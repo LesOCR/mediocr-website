f885cfed12ca90766060fca1c098eec6a300d8c5
Manuel HUEZ <manuel.huez@gmail.com>
2014-10-08 18:12:25 +0200

    Animated the logo

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,25 @@
 (function() {
-  var $logo_highlight = $('.js-logo_highlight');
 
-
-  var timeout = 0;
-
-  $logo_highlight.hide();
-
-  // This function is ran periodically to update the timeout and fade off the logo if needed
-  setInterval(function() {
-    if (timeout == 0)
-      $logo_highlight.fadeOut(800);
-    else if (timeout < 0)
-      timeout = 0;
-    else
-      timeout -= 50;
-  }, 50);
-
-  // Primary event that shows the logo and sets the timeout for its disappearance
-  $(document).on('mousemove', function(event) {
-    if (timeout <= 0)
-  	  $logo_highlight.fadeIn(500);
-    timeout = 1500;
+  $(document).click(function()
+  {
+      animateLogo();
   });
+
+
+  function animateLogo()
+  {
+      $animateLogo = jQuery('<div/>', {
+          class: 'logo-image-animate',
+      });
+      $animateLogo.appendTo('.logo-container').animate({
+         opacity: 0,
+         height: "168",
+         width: "168",
+         top: "0",
+         left: "0"
+     }, 1000, function()
+     {
+         $animateLogo.remove();
+     });
+  }
 })();
